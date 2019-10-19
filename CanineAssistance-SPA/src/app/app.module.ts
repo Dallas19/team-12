82899be9dd12f94a7b6590b85875d6150e25c09a
DESKTOP-1L2LVO0\eddie <eddie97g@gmail.com>
2019-10-18 22:36:52 -0500

    created binary option component

--- CanineAssistance-SPA/src/app/app.module.ts
+++ CanineAssistance-SPA/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BinaryOptionComponent } from './components/binaryoptions/binary-option.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BinaryOptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ CanineAssistance-SPA/src/app/components/binaryoptions/binary-option.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-binary-option',
+  templateUrl: './binary-option.component.html',
+  styleUrls: ['./binary-option.component.css']
+})
+export class BinaryOptionComponent {
+  title = 'CanineAssistance-SPA';
+}
