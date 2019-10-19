22ea8d38969b30dfd772dc340d0f9ea74d9841d0
DESKTOP-1L2LVO0\eddie <eddie97g@gmail.com>
2019-10-19 01:01:41 -0500

    added more to the medical flow path

--- CanineAssistance-SPA/src/app/app.module.ts
+++ CanineAssistance-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BinaryOptionComponent } from './components/binaryoptions/binary-option.component';
 
 @NgModule({
@@ -13,7 +14,8 @@ import { BinaryOptionComponent } from './components/binaryoptions/binary-option.
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
